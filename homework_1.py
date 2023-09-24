ff48c971c90e200d4bb738fc3c791e323411ce06
Apolo0690 <afamilevic@gmail.com>
2023-09-24 20:14:43 +0300

    homework_1

---
+++ homework_1.py
@@ -0,0 +1,45 @@
+# Task_1
+# Задача: Створіть дві змінні first=10, second=30.
+# Виведіть на екран результат математичної взаємодії
+# (+, -, *, / и тд.) для цих чисел.
+
+first = 10
+second = 30
+
+math_operations = first + second, first - second
+print(math_operations)
+
+math_operations = first * second, first / second
+print (math_operations)
+
+math_operations = first % second, first // second
+print (math_operations)
+
+math_operations = first ** second
+print (math_operations)
+
+
+
+# Task_2
+# Задача: Створіть змінну і почергово запишіть в неї результат порівняння
+# (<, > , ==, !=) чисел з завдання 1. Виведіть на екран результат кожного порівняння.
+
+equality = first > second, first < second
+print(equality)
+
+equality = first <= second, first >= second
+print(equality)
+
+equality = first == second, first != second
+print(equality)
+
+
+# # Task_3
+# Задача: Створіть змінну - результат конкатенації (складання)
+# строк str1="Hello " и str2="world". Виведіть на екран.
+
+str_1 = 'Hello'
+str_2 = 'world'
+
+addition = str_1 + ' ' + str_2
+print(addition)
